959e420f53a0db7e8a8b78343cbeeaecfc5983b1
FREEDOM <freewhynane62@gmail.com>
2023-04-08 08:51:07 +0000

    Add React Router and Quiz functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Home, QuizPage, Result } from "./Pages";
+import Header from "./components/Header/Header";
 
 function App() {
+  const [name, setName] = useState("");
+  const [questions, setQuestions] = useState();
+  const [score, setScore] = useState(0);
+
+  async function fetchQuestions(category, difficulty) {
+    try {
+      const response = await fetch(
+        `https://opentdb.com/api.php?amount=15${
+          category ? `&category=${category}` : ""
+        }${difficulty ? `&difficulty=${difficulty}` : ""}&type=multiple`
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+
+      setQuestions(data.results);
+    } catch (error) {
+      console.error("Error fetching questions:", error);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App" style={{ backgroundColor: "#f9fafb" }}>
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home
+                name={name}
+                setName={setName}
+                fetchQuestions={fetchQuestions}
+              />
+            }
+          ></Route>
+          <Route
+            path="/quiz"
+            element={
+              <QuizPage
+                name={name}
+                questions={questions}
+                score={score}
+                setScore={setScore}
+                setQuestions={setQuestions}
+              />
+            }
+          ></Route>
+          <Route
+            path="/result"
+            element={<Result name={name} score={score} questions={questions} />}
+          ></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
