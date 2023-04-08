2a8061b8a1d12b44b314db20c0c57f08ee654d20
FREEDOM <freewhynane62@gmail.com>
2023-04-08 08:47:41 +0000

    add errormessage component

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
-const ErrorMessage = () => {
-  return <div>ErrorMessage</div>;
+const ErrorMessage = ({ children }) => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        padding: 10,
+        marginBottom: 10,
+        borderRadius: 10,
+        color: "#ffffff",
+        backgroundColor: "orange",
+        textAlign: "center",
+        textTransform: "capitalize",
+      }}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default ErrorMessage;
