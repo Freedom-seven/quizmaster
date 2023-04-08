385be9a74c1e726c49bf3d70dac20fd29be3f929
FREEDOM <freewhynane62@gmail.com>
2023-04-08 09:04:52 +0000

    add Result component with functionalities

--- src/Pages/Result/Result.jsx
+++ src/Pages/Result/Result.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { Button } from "@material-ui/core";
+import React, { useEffect, useMemo } from "react";
+import { useNavigate } from "react-router-dom";
+import "./ResultStyles.css";
 
-const Result = () => {
-  return <div>Result</div>;
+const Result = ({ name, score, questions }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!name) {
+      navigate("/");
+    }
+  }, [name, navigate]);
+
+  const averageScore = useMemo(() => {
+    return questions.length / 2;
+  }, [questions]);
+
+  return (
+    <div className="result">
+      <div>
+        <h3>
+          {score > averageScore ? "Well done" : "Not too bad"}, {name}
+        </h3>
+        <span className="score-info">
+          You scored {score} out of {questions.length} questions.
+        </span>
+      </div>
+      <Button
+        href="/"
+        color="secondary"
+        size="large"
+        variant="contained"
+        style={{ alignSelf: "center", margin: 30 }}
+      >
+        Back to Homepage
+      </Button>
+    </div>
+  );
 };
 
 export default Result;
