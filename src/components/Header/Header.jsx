e8de8f449994f7f39832cded1710e04a359d1b66
FREEDOM <freewhynane62@gmail.com>
2023-04-08 08:54:45 +0000

    add Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <Link to="/" className="title">
+        Quiz Master
+      </Link>
+    </div>
+  );
 };
 
 export default Header;
